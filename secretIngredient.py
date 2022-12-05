43ab26388f57cd3765506490e616c77b1ba63244
bentx <bentx>
2022-12-05 19:34:55 +0530

    remove VA check in formula

--- secretIngredient.py
+++ secretIngredient.py
@@ -17,7 +17,7 @@ import talib
 import pandas_ta as ta
 
 def movingAverageFormula(ha_df,currIndex,preHigh,preLow,high,low):
-    if(int(ha_df.at[currIndex, '1WVA'])>8000000 and float(preHigh)>=float(preLow) and float(high)<=float(low) ):
+    if( float(preHigh)>=float(preLow) and float(high)<=float(low) ):
         
         return True      
     return False       
@@ -26,23 +26,23 @@ def OpenPercentageGap(prevclose,open,av):
     return float(prevclose)>float(open) and int(av)>50000 and stockFormula.percentageCalc(prevclose,open)>2 and  stockFormula.percentageCalc(prevclose,open)<8
 
 def WA1Stratagy1(ha_df,currIndex):
-    if (int(ha_df.at[currIndex, '1WVA'])>8000000 and  "RRRG1"==dataUtility.findCandleType(ha_df,currIndex-3)+dataUtility.findCandleType(ha_df,currIndex-2)+dataUtility.findCandleType(ha_df,currIndex-1)+dataUtility.findCandleType(ha_df,currIndex) )and (dataUtility.isTouchLow(ha_df,currIndex-1) or (dataUtility.isTouchLow(ha_df,currIndex-2))):
+    if ( "RRRG1"==dataUtility.findCandleType(ha_df,currIndex-3)+dataUtility.findCandleType(ha_df,currIndex-2)+dataUtility.findCandleType(ha_df,currIndex-1)+dataUtility.findCandleType(ha_df,currIndex) )and (dataUtility.isTouchLow(ha_df,currIndex-1) or (dataUtility.isTouchLow(ha_df,currIndex-2))):
         return True
     return False
 
 
 def WA1Stratagy2(ha_df,currIndex):
-    if (int(ha_df.at[currIndex, '1WVA'])>8000000 and  "RRRR1"==dataUtility.findCandleType(ha_df,currIndex-3)+dataUtility.findCandleType(ha_df,currIndex-2)+dataUtility.findCandleType(ha_df,currIndex-1)+dataUtility.findCandleType(ha_df,currIndex) )and (dataUtility.isTouchLow(ha_df,currIndex-1) or (dataUtility.isTouchLow(ha_df,currIndex-2))) and not (dataUtility.isTouchLow(ha_df,currIndex) ):
+    if ( "RRRR1"==dataUtility.findCandleType(ha_df,currIndex-3)+dataUtility.findCandleType(ha_df,currIndex-2)+dataUtility.findCandleType(ha_df,currIndex-1)+dataUtility.findCandleType(ha_df,currIndex) )and (dataUtility.isTouchLow(ha_df,currIndex-1) or (dataUtility.isTouchLow(ha_df,currIndex-2))) and not (dataUtility.isTouchLow(ha_df,currIndex) ):
         return True
     return False
 
 def bodyTouch(ha_df,currIndex,ma):
-     if (int(ha_df.at[currIndex, '1WVA'])<4000000 and float(ha_df.at[currIndex, ma])>float(ha_df.at[currIndex, 'open']) and float(ha_df.at[currIndex, ma]) < float(ha_df.at[currIndex, 'close']) and "GGG"==dataUtility.findCandleType(ha_df,currIndex-2)+dataUtility.findCandleType(ha_df,currIndex-1)+dataUtility.findCandleType(ha_df,currIndex) ):
+     if (float(ha_df.at[currIndex, ma])>float(ha_df.at[currIndex, 'open']) and float(ha_df.at[currIndex, ma]) < float(ha_df.at[currIndex, 'close']) and "GGG"==dataUtility.findCandleType(ha_df,currIndex-2)+dataUtility.findCandleType(ha_df,currIndex-1)+dataUtility.findCandleType(ha_df,currIndex) ):
         return True 
      return False
 
 def wigTouch(ha_df,currIndex,ma):
-     if (int(ha_df.at[currIndex, '1WVA'])<4000000 and float(ha_df.at[currIndex, ma])>float(ha_df.at[currIndex, 'low']) and float(ha_df.at[currIndex, ma]) < float(ha_df.at[currIndex, 'open']) and "R1G1"==dataUtility.findCandleType(ha_df,currIndex-1)+dataUtility.findCandleType(ha_df,currIndex) ):
+     if (float(ha_df.at[currIndex, ma])>float(ha_df.at[currIndex, 'low']) and float(ha_df.at[currIndex, ma]) < float(ha_df.at[currIndex, 'open']) and "R1G1"==dataUtility.findCandleType(ha_df,currIndex-1)+dataUtility.findCandleType(ha_df,currIndex) ):
         return True
      return False
 
