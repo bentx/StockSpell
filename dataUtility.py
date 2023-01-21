f6ea68258bd2196fff1713c6bf9e6f128b3bb070
bentx <bentx>
2023-01-21 20:20:01 +0530

    fix

--- dataUtility.py
+++ dataUtility.py
@@ -22,7 +22,10 @@ def getStockData(stockCode,timeList):
         #todate = datetime.today()-timedelta(days=365)
         todate = datetime.today()
         fromdate = todate - timedelta(days=timeLine[1])
-        response = requests.get("https://priceapi.moneycontrol.com/techCharts/indianMarket/stock/history?symbol="+stockCode+"&resolution="+timeLine[0]+"&from="+str(int (time.mktime(fromdate.timetuple())))+"&to="+str(int (time.mktime(todate.timetuple()))))
+        response = requests.get("https://priceapi.moneycontrol.com/techCharts/indianMarket/stock/history?symbol=MSUMI&resolution=1D&from=1634083200&to=1673827200&countback=365&currencyCode=INR",headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+)      
+
+        #response = requests.get("https://priceapi.moneycontrol.com/techCharts/indianMarket/stock/history?symbol="+stockCode+"&resolution="+timeLine[0]+"&from="+str(int (time.mktime(fromdate.timetuple())))+"&to="+str(int (time.mktime(todate.timetuple()))),headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"})
         data.append([timeLine[0],response.json()])
     return data
     
--- top10.py
+++ top10.py
@@ -6,6 +6,7 @@ import dataUtility
 import stockFormula
 import os
 import numpy as np
+from datetime import datetime, timedelta
 
 def getTop(res12,res6,res3,date):
     top50=res12.loc
@@ -16,74 +17,94 @@ def getRolling(df,n):
 def process(mtl,ret_12,ret_6,ret_3,Time):
     for date in mtl.index[:-1]:
             stock_list=stockFormula.get_top10(ret_12,ret_6,ret_3,date)
-            for i in range(0,10):
-                dataUtility.storeInFile("./Results/top10Analysis/top10"+Time+".csv",[date,stock_list[i],str(mtl.loc[date:,stock_list[i]][1:2].values[0])])
+            for stock in stock_list:
+                dataUtility.storeInFile("./Results/top10Analysis/top10"+Time+".csv",[date,stock,str(mtl.loc[date:,stock][1:2].values[0])])
 
-def processdaily(mtl,ret_12,ret_6,ret_3,Time):
+def processdaily(mtl,ret_12,ret_6,ret_3,Time,copy):
     for date in mtl.index[:-11]:
             stock_list=stockFormula.get_top10(ret_12,ret_6,ret_3,date)
-            for i in range(0,10):
+            for stock in stock_list:
                 result="Fail"
                 val=0
+                index=0
                 for j in range(1,11):
-                    if(float(mtl.loc[date:,stock_list[i]][j:j+1].values[0])>=1.01):
+                    change=float(copy.loc[date:,stock][j:j+1].values[0])-float(copy.loc[date:,stock][0:1].values[0])
+                    if((10000/float(copy.loc[date:,stock][j:j+1].values[0])*change)>=100):
                         result="Pass"
-                        val=mtl.loc[date:,stock_list[i]][j:j+1].values[0]
+                        val=mtl.loc[date:,stock][j:j+1].values[0]
+                        index=j
                         break
-                dataUtility.storeInFile("./Results/top10Analysis/top10"+Time+".csv",[date,stock_list[i],result,val])
+                dataUtility.storeInFile("./Results/top10Analysis/top10"+Time+".csv",[date,stock,result,val,index])
 
-def processdailyWithStratagy(mtl,ret_12,ret_6,ret_3,stockList,stratagy):
-    for date in mtl.index[:-11]:
-            stock_list=stockFormula.get_top10_WithStratagy(ret_12,ret_6,ret_3,date,stockList)
-            shape = stock_list.shape
-            for i in range(0,shape[1]):
-                result="Fail"
-                val=0
-                for j in range(1,11):
-                    if(float(mtl.loc[date:,stock_list[i]][j:j+1].values[0])>=1.01):
-                        result="Pass"
-                        val=mtl.loc[date:,stock_list[i]][j:j+1].values[0]
-                        break
-                dataUtility.storeInFile("./Results/top10AnalysisStratagy/top10"+stratagy,[date,stock_list[i],result,val])
-
+def processdailyWithStratagy(mtl,ret_12,ret_6,ret_3,stockList,stratagy,date,copy):
+            date=datetime.strptime(date, '%b %d %Y %I:%M%p')
+            strdate=date.strftime("%Y-%m-%d")
+            try:
+                stock_list=stockFormula.get_top10_WithStratagy(ret_12,ret_6,ret_3,strdate,stockList)
+                for stock in stock_list:
+                    result="Fail"
+                    val=0
+                    index=0
+                    for j in range(1,11):
+                        change=float(copy.loc[date:,stock][j:j+1].values[0])-float(copy.loc[date:,stock][0:1].values[0])
+                        if((10000/float(copy.loc[date:,stock][j:j+1].values[0])*change)>=100):
+                            result="Pass"
+                            val=mtl.loc[date:,stock][j:j+1].values[0]
+                            index=j
+                            break
+                    dataUtility.storeInFile("./Results/top10AnalysisStratagy/top10"+stratagy,[date,stock,result,val,index])
+            except IndexError as e:
+                    print("OOPS Index Error")
+            except KeyError as e:
+                    print("OOPS Key Error"+e)
 
 def getTopAmongStratagy(rootdf):
+    rootdf =rootdf.fillna(method='bfill')
+    rootcopy=rootdf.copy()
+    dtl=(rootdf.pct_change()+1)[1:]
+    dret_12,dret_6,dret_3 = stockFormula.get_rolling_ret(dtl,12),stockFormula.get_rolling_ret(dtl,6),stockFormula.get_rolling_ret(dtl,3)
     path = "./Results/details/"
     file_list = os.listdir(path) 
-    for file in file_list:
-        overalldata={}
-        with open(f'./Results/details/{file}') as csv_file:
-            print(f'Top10Processing {file}')
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for result in csv_reader:
-                if result[0] in overalldata:
-                    overalldata[result[0]].append(result[2])
-                else:
-                    overalldata[result[0]]=[]
-                    overalldata[result[0]].append(result[2])
-        for date in overalldata:
-            dtl=(rootdf.pct_change()+1)[1:]
-            dret_12,dret_6,dret_3 = stockFormula.get_rolling_ret(dtl,12),stockFormula.get_rolling_ret(dtl,6),stockFormula.get_rolling_ret(dtl,3)
-            processdailyWithStratagy(dtl,dret_12,dret_6,dret_3,overalldata[date],file)
-
+    try:
+        for file in file_list:
+            overalldata={}
+            with open(f'./Results/details/{file}') as csv_file:
+                print(f'Top10Processing {file}')
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                for result in csv_reader:
+                    if result[0] in overalldata:
+                        overalldata[result[0]].append(result[2])
+                    else:
+                        overalldata[result[0]]=[]
+                        overalldata[result[0]].append(result[2])
+            for date in overalldata:
+                processdailyWithStratagy(dtl,dret_12,dret_6,dret_3,overalldata[date],file,date,rootcopy)
+    except IndexError as e:
+                    print("OOPS Index Error getTopAmongStratagy")
+    except KeyError as e:
+                    print("OOPS Key Error getTopAmongStratagy"+e)
 
 
 
 
 def getTopScorers(rootdf):
-    rootdf =rootdf.dropna(axis=1)
+    #rootdf =rootdf.dropna(axis=1)
+    rootdf =rootdf.fillna(method='bfill')
+    rootcopy=rootdf.copy()
+
+    print(rootdf)
     mtl=(rootdf.pct_change()+1)[1:].resample('M').prod()
     ret_12,ret_6,ret_3 = stockFormula.get_rolling_ret(mtl,12),stockFormula.get_rolling_ret(mtl,6),stockFormula.get_rolling_ret(mtl,3)
     process(mtl,ret_12,ret_6,ret_3,"Month")
 
     dtl=(rootdf.pct_change()+1)[1:]
     dret_12,dret_6,dret_3 = stockFormula.get_rolling_ret(dtl,12),stockFormula.get_rolling_ret(dtl,6),stockFormula.get_rolling_ret(dtl,3)
-    processdaily(dtl,dret_12,dret_6,dret_3,"Day")
+    processdaily(dtl,dret_12,dret_6,dret_3,"Day",rootcopy)
 
     dtl3=(rootdf.pct_change()+1)[1:].resample('3D').prod()
     dret3_12,dret3_6,dret3_3 = stockFormula.get_rolling_ret(dtl3,12),stockFormula.get_rolling_ret(dtl3,6),stockFormula.get_rolling_ret(dtl3,3)
-    processdaily(dtl3,dret3_12,dret3_6,dret3_3,"Day3")
+    processdaily(dtl3,dret3_12,dret3_6,dret3_3,"Day3",rootcopy)
 
     dtl5=(rootdf.pct_change()+1)[1:].resample('5D').prod()
     dret5_12,dret5_6,dret5_3 = stockFormula.get_rolling_ret(dtl5,12),stockFormula.get_rolling_ret(dtl5,6),stockFormula.get_rolling_ret(dtl5,3)
-    processdaily(dtl5,dret5_12,dret5_6,dret5_3,"Day5")
+    processdaily(dtl5,dret5_12,dret5_6,dret5_3,"Day5",rootcopy)
